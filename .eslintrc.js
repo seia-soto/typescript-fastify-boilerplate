c3d83da1dde0d1eeb0b0dfc3ef9b1fef9c82ce49
seia-soto <seia@outlook.kr>
2021-10-17 00:29:12 +0900

    chore: upgrade packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    node: true
   },
   extends: [
     'standard'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 13,
     sourceType: 'module'
   },
   plugins: [
     '@typescript-eslint'
-  ]
+  ],
+  rules: {
+  }
 }
